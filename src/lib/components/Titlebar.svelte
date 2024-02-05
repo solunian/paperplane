2f6cd5c24a2bec2882e900b1d62a73aa9651609f
Daniel Cui <solunian@gmail.com>
2024-02-04 23:00:36 -0800

    fix: cut weird dbclick maximize quirk

--- src/lib/components/Titlebar.svelte
+++ src/lib/components/Titlebar.svelte
@@ -1,19 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { Window } from "@tauri-apps/api/window";
-
-  let bar: HTMLDivElement;
-
-  onMount(() => {
-    bar.addEventListener("dblclick", async (event: MouseEvent) => {
-      const window = Window.getCurrent();
-      if (!window.isMaximized()) {
-        window.maximize();
-      } else {
-        window.unmaximize();
-      }
-    });
-  });
 </script>
 
-<div bind:this={bar} data-tauri-drag-region class="h-7 bg-gray-500" />
+<div data-tauri-drag-region class="h-7 bg-gray-500" />
